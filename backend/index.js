e64c3ebc8fe6bab21059d47959aa4a0e5c51b79d
isukanimalasuriya <anujitha1234@gmail.com>
2026-02-25 21:12:59 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB(); // call database connection
 
 const app = express();
-const PORT = 5000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is running...");
+  res.send("API Running...");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
