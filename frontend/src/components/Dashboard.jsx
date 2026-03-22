0ec6eb59d9493a10086b3c2a0ff42469e33cb6e3
sandeepasubashini <sandeepasubashini6@gmail.com>
2026-03-22 14:48:46 +0530

    Remove unused import of useNavigate from Dashboard component

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import {
   Search,
   Bell,
